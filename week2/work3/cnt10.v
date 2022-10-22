10087d83b7d1f5393d937b0b3d4c3b5769811a45
RoxyKko <njy_roxy@outlook.com>
2022-10-23 02:36:23 +0800

    :tada:十进制计数器

---
+++ week2/work3/cnt10.v
@@ -0,0 +1,50 @@
+module cnt10 (
+    reset,
+    en,
+    clk,
+    q,
+    count
+);
+    
+    input reset;
+    input en;
+    input clk;
+    output reg [3:0] q;
+    output reg [3:0] count;
+
+    always @(posedge clk or posedge reset)
+    begin
+        if(reset)
+        begin
+            q <= 4'b0000;
+        end
+        else if(en)
+        begin
+            if(q == 4'b1001)
+            begin
+                q <= 4'b0000;
+            end
+            else
+            begin
+                q <= q + 1;
+            end
+        end
+    end
+always @(reset or q)
+begin
+    if (reset)
+    begin
+        count <= 4'b0000;
+    end
+    else if (q == 4'b1001)
+    begin
+        count <= count + 1;
+    end
+    else
+    begin
+        count <= count;
+    end
+end
+
+
+endmodule
