f902b7b96057da9a6b22a9a39b287a1e8343dc25
RoxyKko <njy_roxy@outlook.com>
2022-11-02 21:19:01 +0800

    :rocket:仿真不出来结果，什么情况

--- week2/work4/work4-4/div_ctr.v
+++ week2/work4/work4-4/div_ctr.v
@@ -1,81 +1,56 @@
-module div_ctr (
-    reset,
-    clk50m,
-    d,
-    fout
-);
-input reset;
-input clk50m;
-input [7:0] d;
-output reg fout;
+module div_ctr(reset, clk_50m, d, fout);
+    input reset, clk_50m;
+    input [7:0] d;
+    output fout;
 
+    // 50khz timer
+    reg clk_50khz;
+    reg [9:0] cnt;
 
-//产生50khz的时钟
-reg [9:0]cnt10;
-reg clk50khz;
-always @(posedge clk50m or negedge reset)
-begin
-    if(!reset)
-    begin
-        cnt10 <= 10'd0;
-    end
-    else if(cnt10 == 10'd999)
-    begin
-        cnt10 <= 10'd0;
-    end
-    else
-    begin
-        cnt10 <= cnt10 + 1;
-    end
-end
+    always @ (negedge reset, posedge clk_50m)
+        begin
+            if (!reset)
+                cnt <= 0;
+            else
+                begin
+                    if (cnt == 1000 - 1)
+                        cnt <= 0;
+                    else
+                        cnt <= cnt + 1;
+                end
+        end
 
-always @(posedge cnt10 or negedge reset)
-begin
-    if(!reset)
-    begin
-        clk50khz <= 1'b0;
-    end
-    else if(cnt10 <= 10'd499)
-    begin
-        clk50khz <= 1'b1;
-    end
-    else
-    begin
-        clk50khz <= 1'b0;
-    end
-end
+    always @ (negedge reset, posedge clk_50m)
+        begin
+            if (!reset)
+                clk_50khz <= 0;
+            else if (cnt < 1000 / 2)
+                clk_50khz <= 1;
+            else
+                clk_50khz <= 0;
+        end
 
-//产生数控分频时钟
+    // 数控分频器
+    reg fout;
+    reg cout;
+    reg [7:0] cnt8;
+    always @ (posedge clk_50khz)
+        begin
+            if (cnt8 == 8'b11111111)
+                cnt8 <= d;
+            else
+                cnt8 <= cnt8 + 1;
+        end
 
-reg cout;
-reg [7:0]cnt8;
-always @(posedge clk50khz)
-begin
-    if(cnt8 == 8'd255)
-    begin
-        cnt8 <= d;
-    end
-    else
-    begin
-        cnt8 <= cnt8 + 1;
-    end
-end
+    always @ (posedge clk_50khz)
+        begin
+            if (cnt8 == 8'b11111110)
+                cout <= 1;
+            else
+                cout <= 0;
+        end
 
-always @(posedge clk50khz)
-begin
-    if(cnt8 == 8'd254)
-    begin
-        cout <= 1'b1;
-    end
-    else
-    begin
-        cout <= 0;
-    end
-end
-
-always @(posedge cout)
-begin
-    fout <= ~fout;
-end
-
+    always @ (posedge cout)
+        fout <= ~fout;
 endmodule
+
